f206d5947fdfb9dcaa46bd295f25b8d6ee487226
ajarek <ajarek@poczta.onet.pl>
2022-08-21 01:34:01 +0200

    initial code

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+class Slider extends React.Component {
+    render(){
+        return(
+        <div>
+          <img src="/images/foto01.png" alt=""  />
+        </div>
+        )
+        }
+}
+export default Slider;
